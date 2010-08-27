2e1e1cb42b08bfa309769627e911817731b1c446
PA Parent <paparent@paparent.me>
2010-08-27 19:17:10 -0400

    Added Object ID in the Object Panel
    Closed issue #2

--- static/js/App.ObjectPanel.js
+++ static/js/App.ObjectPanel.js
@@ -1,11 +1,14 @@
 App.ObjectPanel = Ext.extend(Ext.Panel, {
 	border: false
 	,closable: true
-	,layout: 'fit'
+	,layout: 'vbox'
 	,initComponent: function(){
+		this.items = [
+			{border:false,layout:'fit',bodyStyle:'padding:5px;',html:"Object ID: " + this.oid}
+			,new App.TagValuesGrid({oid: this.oid})
+		];
+		this.title = 'Object ' + this.oid;
 		App.ObjectPanel.superclass.initComponent.call(this);
-		this.setTitle('Object ' + this.oid);
-		this.add(new App.TagValuesGrid({oid: this.oid}));
 	}
 });
 
