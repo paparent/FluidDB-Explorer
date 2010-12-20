ea43e81b99fc18a9fd87f9793e308d9f403fb405
PA Parent <paparent@paparent.me>
2010-12-20 15:50:05 -0500

    Changed the login button text

--- fluiddbexplorer/static/js/App.Login.js
+++ fluiddbexplorer/static/js/App.Login.js
@@ -1,16 +1,25 @@
 App.Login = Ext.extend(Ext.Button, {
-	text: 'Logged as: ' + App.Config.username
+	text: 'Log in'
 	,initComponent: function(){
 		App.Login.superclass.initComponent.call(this);
+		this.updateText(App.Config.username);
 	}
 	,handler: function(btn){
 		var w = new App.LoginWindow();
 		w.show();
 		w.on('logged', function(username){
 			w.destroy();
-			btn.setText('Logged as: ' + username);
-			btn.ownerCt.doLayout();
+			btn.updateText(username);
 		});
+	}
+	,updateText: function(username){
+		if (username == 'Anonymous') {
+			this.setText('Log in with your FluidDB ID');
+		}
+		else {
+			this.setText('Logged in as: ' + username);
+		}
+		this.ownerCt.doLayout();
 	}
 });
 
