75255b0fd22f92ae092345f5c42034eb37260302
PA Parent <paparent@paparent.me>
2011-01-10 08:32:15 -0500

    Added runserver.py script

---
+++ runserver.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    runserver
+    ~~~~~~~~~
+
+    Run server
+
+    :copyright: 2010 by FluidDB Explorer Authors
+    :license: MIT, see LICENSE for more information
+"""
+
+from fluiddbexplorer import app
+app.run(host='0.0.0.0')
