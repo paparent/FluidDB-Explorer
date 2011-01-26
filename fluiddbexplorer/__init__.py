ec56e88328d588dbf90cd54bee2bca9755d99b41
PA Parent <paparent@paparent.me>
2011-01-26 15:14:17 -0500

    add timestamp to static files url

--- fluiddbexplorer/__init__.py
+++ fluiddbexplorer/__init__.py
@@ -7,6 +7,7 @@
     :license: MIT, see LICENSE for more information
 """
 
+import os
 from flask import Flask, abort, redirect, render_template, session, url_for
 
 from flaskext.extdirect import ExtDirect
@@ -18,6 +19,21 @@ extdirect = ExtDirect(app)
 
 
 from fluiddbexplorer import direct
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 @app.route('/favicon.ico/')
